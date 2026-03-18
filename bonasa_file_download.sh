26f41047b36fab9a9052735cd969fb965c3719d1
Luke Scott <lukescottarthur@gmail.com>
2026-03-18 16:42:55 -0400

    workin

--- bonasa_file_download.sh
+++ bonasa_file_download.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=bonasa_file_download                      # Job name 
 #SBATCH --partition=batch                           # Partition name 
 #SBATCH --ntasks=1                                  # 1 task (process)
-#SBATCH --cpus-per-task=8                           # CPU core count per task
+#SBATCH --cpus-per-task=16                           # CPU core count per task
 #SBATCH --mem=24G                                    # Memory per node
 #SBATCH --time=03:00:00                              # Time limit hrs:mins:secs
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
@@ -25,25 +25,6 @@ then
     mkdir -p $OUTDIR
 fi
 
-
 # Download reads
 prefetch -O $OUTDIR --option-file bonasa_SRR.numbers
 
-
-
-
-
-
-
-
-# Map reads to reference and store in bam format
-bwa mem -t 6 Bumbellus.assembly.fa SRR8082143_1.fastq SRR8082143_2.fastq | samtools sort -O BAM --threads 6 - > SRR25747755.sorted.bam
-
-# Index bam file
-samtools index --threads 6 SRR8082143.sorted.bam
-
-# Compute genotype likelihoods (map quality > 60; quality score > 40; mapped reads > 10)
-bcftools mpileup -Ou --threads 6 --min-MQ 60 -f ecoli_MG1655.fasta SRR8082143.sorted.bam | bcftools call --threads 6 -mv -Ou --ploidy 1 - | bcftools filter -Oz -e 'QUAL<40 || DP<10' > SRR8082143_Final.vcf.gz
-
-# Generate index file
-bcftools index SRR8082143_Final.vcf.gz
