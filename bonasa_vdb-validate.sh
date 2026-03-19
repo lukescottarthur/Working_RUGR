eee0b9c1fa5d45441593a1201bee692f77c6097c
Luke Scott <lukescottarthur@gmail.com>
2026-03-19 09:52:21 -0400

    removing files and such

---
+++ bonasa_vdb-validate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=bonasa_vdb-validate                      # Job name 
+#SBATCH --partition=batch                           # Partition name 
+#SBATCH --ntasks=1                                  # 1 task (process)
+#SBATCH --cpus-per-task=8                           # CPU core count per task
+#SBATCH --mem=16G                                    # Memory per node
+#SBATCH --time=02:00:00                              # Time limit hrs:mins:secs
+#SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
+#SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
+#SBATCH --mail-user=las80898@uga.edu                # Where to send mail
+#SBATCH --mail-type=BEGIN,END,FAIL                        # Mail events 
+
+# Load conda environment (SRA toolkit, BWA, samtools, bcftools)
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate bonasa_env
+
+OUTDIR="/home/las80898/bonasa/reads"
+
+# Change directory into output directory
+cd $OUTDIR
+
+# cycle through directories and use vdb-validate
+for i in $(/home/las80898/bonasa/reads)
+do
+cd SRR2
+vdb-validate
