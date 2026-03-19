30868ce45bd986f3ae531e9f1128d1c8759c8816
Luke Scott <lukescottarthur@gmail.com>
2026-03-19 08:50:42 -0400

    trying busco again and leftover download

--- bonasa_file_download_leftovers.sh
+++ bonasa_file_download_leftovers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=bonasa_busco                      # Job name 
+#SBATCH --job-name=bonasa_file_download_leftovers                      # Job name 
 #SBATCH --partition=batch                           # Partition name 
 #SBATCH --ntasks=1                                  # 1 task (process)
 #SBATCH --cpus-per-task=16                           # CPU core count per task
-#SBATCH --mem=32G                                    # Memory per node
-#SBATCH --time=10:00:00                              # Time limit hrs:mins:secs
+#SBATCH --mem=8G                                    # Memory per node
+#SBATCH --time=01:00:00                              # Time limit hrs:mins:secs
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
 #SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
 #SBATCH --mail-user=las80898@uga.edu                # Where to send mail
@@ -17,7 +17,7 @@ conda activate bonasa_env
 
 # configure SRA toolkit before running script with 'vdb-config -i'
 
-OUTDIR="/scratch/las80898/bonasa_busco_2"
+OUTDIR="/home/las80898/bonasa/reads"
 
 # If output directory doesn't exist, create it
 if [ ! -d $OUTDIR ]
@@ -25,11 +25,5 @@ then
     mkdir -p $OUTDIR
 fi
 
-# Change directory into output directory
-cd $OUTDIR
-
-# Copy assembly into OUTDIR
-cp /home/las80898/bonasa/Bumbellus.assembly.fa $OUTDIR
-
-# Run busco
-busco -i Bumbellus.assembly.fa -o busco_test_1 -f --cpu 8 --mode genome -l galloanserae_odb12 --augustus --augustus_species chicken
+# Download reads
+prefetch -O $OUTDIR SRR25747803
