b92b8300cd235f2a09c351c9da2d7345986ec56a
Luke Scott <lukescottarthur@gmail.com>
2026-03-19 14:56:43 -0400

    update fastqdump script

--- bonasa_fastq-dump.sh
+++ bonasa_fastq-dump.sh
@@ -2,13 +2,15 @@
 #SBATCH --job-name=bonasa_fastq-dump                      # Job name 
 #SBATCH --partition=batch                           # Partition name 
 #SBATCH --ntasks=1                                  # 1 task (process)
-#SBATCH --cpus-per-task=8                           # CPU core count per task
+#SBATCH --cpus-per-task=6                           # CPU core count per task
 #SBATCH --mem=16G                                    # Memory per node
 #SBATCH --time=04:00:00                              # Time limit hrs:mins:secs
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
 #SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
 #SBATCH --mail-user=las80898@uga.edu                # Where to send mail
 #SBATCH --mail-type=BEGIN,END,FAIL                        # Mail events 
+
+# default threads=6 for fastq-dump.
 
 # Load conda environment
 CONDA_BASE=$(conda info --base)
