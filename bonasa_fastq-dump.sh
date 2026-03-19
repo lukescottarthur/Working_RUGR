92894dafa76891f8bb1598c8c7a7b6c8a9a685d5
Luke Scott <lukescottarthur@gmail.com>
2026-03-19 11:33:12 -0400

    vdbvalidate

--- bonasa_fastq-dump.sh
+++ bonasa_fastq-dump.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=bonasa_vdb-validate                      # Job name 
+#SBATCH --job-name=bonasa_fastq-dump                      # Job name 
 #SBATCH --partition=batch                           # Partition name 
 #SBATCH --ntasks=1                                  # 1 task (process)
 #SBATCH --cpus-per-task=8                           # CPU core count per task
 #SBATCH --mem=16G                                    # Memory per node
-#SBATCH --time=02:00:00                              # Time limit hrs:mins:secs
+#SBATCH --time=04:00:00                              # Time limit hrs:mins:secs
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
 #SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
 #SBATCH --mail-user=las80898@uga.edu                # Where to send mail
@@ -23,5 +23,5 @@ cd $OUTDIR
 # cycle through directories and use vdb-validate
 for SRR in $OUTDIR
 do
-vdb-validate SRR*
+fastq-dump --split-3 SRR*
 done
