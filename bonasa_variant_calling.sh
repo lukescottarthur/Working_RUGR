4f251b3b0da45f542122bb072523f8ac0fc7151e
Luke Scott <lukescottarthur@gmail.com>
2026-04-13 10:32:41 -0400

    make temp files to reheader

--- bonasa_variant_calling.sh
+++ bonasa_variant_calling.sh
@@ -27,10 +27,18 @@ cd /home/las80898/bonasa/sorted_bam_files
 # Compute genotype likelihoods, variant call, and filter (map quality > 60; quality score > 40; mapped reads > 10)
 for bam in *.bam; do
   sample="${bam%.bam}"
+  tmp=$(mktemp --suffix=.bcf)
+
   bcftools mpileup -Ou --threads 8 --min-MQ 60 -f /home/las80898/bonasa/Bumbellus.assembly.fa "$bam" \
+    > "$tmp"
+
+  bcftools reheader \
+    -h <(bcftools view -h "$tmp" | sed 's/Type=Integer/Type=Float/g') \
+    "$tmp" \
     | bcftools call --threads 8 -mv -Ou - \
-    | bcftools filter -e 'QUAL<40 || DP<10' -Ou \
-    | bcftools reheader -h <(bcftools view -h /dev/stdin | sed 's/MQ,Number=1,Type=Integer/MQ,Number=1,Type=Float/g') \
-    -o "${OUTDIR}/${sample}.vcf.gz"
+    | bcftools filter -Oz -e 'QUAL<40 || DP<10' \
+    > "${OUTDIR}/${sample}.vcf.gz"
+
+  rm -f "$tmp"
 done
 
