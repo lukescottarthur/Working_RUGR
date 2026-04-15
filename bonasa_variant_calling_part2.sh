8acf6cc007162162b26635a2f072a2bda64ef4f0
Luke Scott <lukescottarthur@gmail.com>
2026-04-15 16:19:50 -0400

    editing scripts

--- bonasa_variant_calling_part2.sh
+++ bonasa_variant_calling_part2.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-#SBATCH --job-name=bonasa_variant_calling                      # Job name 
-#SBATCH --partition=batch                           # Partition name 
-#SBATCH --ntasks=1                                  # 1 task (process)
-#SBATCH --cpus-per-task=8                           # CPU core count per task
-#SBATCH --mem=256G                                    # Memory per node
-#SBATCH --time=72:00:00                              # Time limit hrs:mins:secs
-#SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
-#SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
-#SBATCH --mail-user=las80898@uga.edu                # Where to send mail
-#SBATCH --mail-type=BEGIN,END,FAIL                        # Mail events 
+#SBATCH --job-name=bonasa_variant_calling
+#SBATCH --partition=batch
+#SBATCH --array=0-53
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=48:00:00
+#SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%A_%a.out
+#SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%A_%a.error
+#SBATCH --mail-user=las80898@uga.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
 
 # Load conda environment
 CONDA_BASE=$(conda info --base)
-source ${CONDA_BASE}/etc/profile.d/conda.sh
+source "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda activate bonasa_env
 
+INDIR="/scratch/las80898/bonasa/reheader_temp_bcf_reads"
 OUTDIR="/home/las80898/bonasa/vcf_reads"
 
-# If output directory doesn't exist, create it
-if [ ! -d "$OUTDIR" ]; then
-    mkdir -p "$OUTDIR"
-fi
+mkdir -p "$OUTDIR"
 
-cd /scratch/las80898/bonasa/reheader_temp_bcf_reads
+# Build an array of all BCF files and select one per SLURM task
+mapfile -t BCF_FILES < <(ls "${INDIR}"/*.bcf.gz)
+BCF="${BCF_FILES[$SLURM_ARRAY_TASK_ID]}"
+SAMPLE=$(basename "$BCF" .bcf.gz)
 
+# Variant call and filter (quality score > 40; mapped reads > 10)
+bcftools call --threads 4 -mv -Ou "$BCF" \
+  | bcftools filter -Oz -e 'QUAL<40 || DP<10' \
+  > "${OUTDIR}/${SAMPLE}.vcf.gz"
 
-# variant call and filter (quality score > 40; mapped reads > 10)
-for bcf in *.bcf.gz; do
-  sample="${bcf%.bcf.gz}"
-bcftools call --threads 8 -mv -Ou - | bcftools filter -Oz -e 'QUAL<40 || DP<10' > "${OUTDIR}/${sample}.vcf.gz"
-done
+# Index the output for downstream tools
+bcftools index --tbi "${OUTDIR}/${SAMPLE}.vcf.gz"
--- bonasa_vcf_reheader.sh
+++ bonasa_vcf_reheader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=bonasa_variant_calling                      # Job name 
+#SBATCH --job-name=bonasa_vcf_reheader                      # Job name 
 #SBATCH --partition=batch                           # Partition name 
 #SBATCH --ntasks=1                                  # 1 task (process)
-#SBATCH --cpus-per-task=8                           # CPU core count per task
-#SBATCH --mem=256G                                    # Memory per node
-#SBATCH --time=72:00:00                              # Time limit hrs:mins:secs
+#SBATCH --cpus-per-task=4                           # CPU core count per task
+#SBATCH --mem=16G                                    # Memory per node
+#SBATCH --time=36:00:00                              # Time limit hrs:mins:secs
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
 #SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
 #SBATCH --mail-user=las80898@uga.edu                # Where to send mail
@@ -22,13 +22,14 @@ if [ ! -d "$OUTDIR" ]; then
     mkdir -p "$OUTDIR"
 fi
 
-cd /scratch/las80898/bonasa/temp_bcf_reads
+cd /scratch/las80898/bonasa/temp_bcf_reads_3
 
 # change to MQ=Float
 for bcf in *.bcf.gz; do
   sample="${bcf%.bcf.gz}"
 
   bcftools reheader \
-    -h <(bcftools view -h "$bcf" | sed 's/MQ,Number=1,Type=Integer/MQ,Number=1,Type=Float/g') > "${OUTDIR}/${sample}.bcf.gz"
+    -h <(bcftools view -h "$bcf" | sed 's/MQ,Number=1,Type=Integer/MQ,Number=1,Type=Float/g') \
+    "$bcf" \
+    -o "${OUTDIR}/${sample}.bcf.gz"
 done
-
