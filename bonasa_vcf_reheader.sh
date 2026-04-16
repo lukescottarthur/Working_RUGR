84a3f36db53a9ad912a32f88c04a3f650dabbb24
Luke Scott <lukescottarthur@gmail.com>
2026-04-16 08:39:41 -0400

    change reheader to array job

--- bonasa_vcf_reheader.sh
+++ bonasa_vcf_reheader.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
-#SBATCH --job-name=bonasa_vcf_reheader                      # Job name 
-#SBATCH --partition=batch                           # Partition name 
-#SBATCH --ntasks=1                                  # 1 task (process)
-#SBATCH --cpus-per-task=4                           # CPU core count per task
-#SBATCH --mem=16G                                    # Memory per node
-#SBATCH --time=36:00:00                              # Time limit hrs:mins:secs
-#SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%j.out  
-#SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%j.error 
-#SBATCH --mail-user=las80898@uga.edu                # Where to send mail
-#SBATCH --mail-type=BEGIN,END,FAIL                        # Mail events 
+#SBATCH --job-name=bonasa_vcf_reheader
+#SBATCH --partition=batch
+#SBATCH --array=0-53
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=36:00:00
+#SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%A_%a.out
+#SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%A_%a.error
+#SBATCH --mail-user=las80898@uga.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
 
 # Load conda environment
 CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate bonasa_env
 
+INDIR="/scratch/las80898/bonasa/temp_bcf_reads_3"
 OUTDIR="/scratch/las80898/bonasa/reheader_temp_bcf_reads"
+mkdir -p "$OUTDIR"
 
-# If output directory doesn't exist, create it
-if [ ! -d "$OUTDIR" ]; then
-    mkdir -p "$OUTDIR"
-fi
+# Build an array of all files, then select one by SLURM task index
+mapfile -t bcf_FILES < <(ls "${INDIR}"/*.bcf.gz | sort)
 
-cd /scratch/las80898/bonasa/temp_bcf_reads_3
+bcf="${bcf_FILES[$SLURM_ARRAY_TASK_ID]}"
+SAMPLE=$(basename "${bcf%_sorted.bcf.gz}")
+
+echo "Processing sample: $SAMPLE (task index: $SLURM_ARRAY_TASK_ID)"
 
 # change to MQ=Float
-for bcf in *.bcf.gz; do
-  sample="${bcf%.bcf.gz}"
 
-  bcftools reheader \
-    -h <(bcftools view -h "$bcf" | sed 's/MQ,Number=1,Type=Integer/MQ,Number=1,Type=Float/g') \
-    "$bcf" \
-    -o "${OUTDIR}/${sample}.bcf.gz"
+bcftools reheader \
+  -h <(bcftools view -h "$bcf" | sed 's/MQ,Number=1,Type=Integer/MQ,Number=1,Type=Float/g') \
+  "$bcf" \
+  -o "${OUTDIR}/${sample}.bcf.gz"
 done
