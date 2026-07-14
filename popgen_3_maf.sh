2b7e1d49c1f47c1b53fdfa5d49baafa3dc6fe220
Luke Scott <lukescottarthur@gmail.com>
2026-07-13 22:45:13 -0400

    fix maf again 7/13

--- popgen_3_maf.sh
+++ popgen_3_maf.sh
@@ -28,35 +28,34 @@ cd $INDIR
 bcftools view \
   --threads ${SLURM_CPUS_PER_TASK} \
   -i 'AC==0 || AC==AN' \
-  cohort_allsites_2.vcf.gz \
+  cohort_allsites.vcf.gz \
   -O z -o $OUTDIR/invariant_2.vcf.gz
 
 tabix $OUTDIR/invariant_2.vcf.gz
-tabix $OUTDIR/cohort_allsites_2.vcf.gz
 
 # MAF filter + genotype rate filter
 # NOTE: change --geno to .1 or .05 for my dataset
 #plink2 --vcf cohort_allsites.vcf.gz --maf 0.05 --geno 0.5 --hwe 1e-6 --make-pgen --out $OUTDIR/cohort_maf_filtered_allsites --allow-extra-chr
 
 plink2 \
-  --vcf cohort_allsites_2.vcf.gz \
+  --vcf cohort_allsites.vcf.gz \
   --maf 0.05 \
   --geno 0.5 \
   --hwe 1e-6 \
   --export vcf bgz \
   --threads ${SLURM_CPUS_PER_TASK} \
   --memory 250000 \
-  --out $OUTDIR/cohort_maf_filtered_allsites_2 \
+  --out $OUTDIR/cohort_maf_filtered_allsites \
   --allow-extra-chr
 
-tabix -p vcf $OUTDIR/cohort_maf_filtered_allsites_2.vcf.gz
+tabix -p vcf $OUTDIR/cohort_maf_filtered_allsites.vcf.gz
 
 cd $OUTDIR
 
 # combine files
 bcftools concat \
   --allow-overlaps \
-  cohort_maf_filtered_allsites_2.vcf.gz invariant_2.vcf.gz \
+  cohort_maf_filtered_allsites.vcf.gz invariant_2.vcf.gz \
   -O z -o cohort_filtered_allsites.vcf.gz
 
 tabix -p vcf cohort_filtered_allsites.vcf.gz
