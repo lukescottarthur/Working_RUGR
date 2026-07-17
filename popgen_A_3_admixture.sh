2970970b8657b463a962270ce8434dd16af1aafa
Luke Scott <lukescottarthur@gmail.com>
2026-07-17 11:45:13 -0400

    admixture and roh minor edits

--- popgen_A_3_admixture.sh
+++ popgen_A_3_admixture.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=164G
-#SBATCH --time=18:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%A.out
 #SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%A.error
 #SBATCH --mail-user=las80898@uga.edu
--- popgen_A_4_roh.sh
+++ popgen_A_4_roh.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --mem=164G
 #SBATCH --time=16:00:00
 #SBATCH --output=/home/las80898/GENE8940_parallel_2/%x_%A.out
 #SBATCH --error=/home/las80898/GENE8940_parallel_2/%x_%A.error
@@ -17,9 +17,10 @@ CONDA_BASE=$(conda info --base)
 source "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda activate popgen_env
 
-OUTDIR="/scratch/las80898/bonasa/popgen"
+INDIR="/scratch/las80898/bonasa/popgen/cohort_files"
+OUTDIR="/scratch/las80898/bonasa/popgen/ROH"
 
-cd $OUTDIR
+cd $INDIR
 
 
 # BCFtools ROH (HMM-based, best for WGS)
